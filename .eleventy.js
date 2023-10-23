8c6a10d2d068dc470970d83785083dc8c393e514
Saneef Ansari <hello@saneef.com>
2023-10-24 02:56:39 +0530

    Adds remote images

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
+/* @ts-check */
 const img2picture = require("eleventy-plugin-img2picture");
 
 const INPUT_DIR = "./src";
 const OUTPUT_DIR = "./dist";
 
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(img2picture, {
     eleventyInputDir: INPUT_DIR,
     imagesOutputDir: OUTPUT_DIR,
     urlPath: "",
     formats: ["avif"],
+    fetchRemote: true,
     sharpAvifOptions: {
       lossless: true,
       // quality: 1,
--- src/index.md
+++ src/index.md
@@ -6,3 +6,7 @@ title: Testing eleventy-plugin-img2picture
 Hello world
 
 ![Shapes](./shapes.png)
+
+![External image](https://raw.githubusercontent.com/saneef/eleventy-plugin-img2picture/main/tests/fixtures/images/shapes.png)
+
+![External image with query](https://raw.githubusercontent.com/saneef/eleventy-plugin-img2picture/main/tests/fixtures/images/shapes.png?v=1234)
